b7f94505f10a4b1ae17b0c207c93d7a26723d799
oakerekan <gaiusanchez16@gmail.com>
2023-12-23 22:57:22 +0100

    updated readme 1.1.7

--- expense.py
+++ expense.py
@@ -1,23 +1,21 @@
-import uuid 
+from uuid import uuid4
 from datetime import datetime, timezone
 
 class Expense:
-    """ A class representing a single expense, with its title, amount, created_at and updated_at. The id is generated automatically using the uuid library.
-    The created_at and updated_at are generated automatically using the datetime library and contain the current date and time in UTC. The Class expense 
-    contain an __init__ method that takes the title and amount as parameters and sets the id, title, amount, created_at and updated_at attributes. 
-    also contain an update method that takes the title and amount as parameters and updates the title and amount attributes. The to_dict method returns a  dictionary of the set of values inputted after the update method is called.   
-    """
+
 
     def __init__(self, title, amount):
-        """ A method that takes the title and amount as parameters and sets the id, title, amount, created_at and updated_at attributes."""
-        self.id = str(uuid.uuid4())
+        """ A method that takes the title and amount as parameters and sets the id, title, amount, created_at and updated_at attributes.
+        """
+        self.id = str(uuid4())
         self.title = title
         self.amount = amount
         self.created_at = datetime.now(timezone.utc)
         self.updated_at = self.created_at
 
     def update(self, title=None, amount=None):
-        """ A method that takes the title and amount as parameters and updates the title and amount attributes.If the update method is called without any parameters, the title and amount attributes remain unchanged. The updated_at attribute is updated to the current date and time in UTC."""
+        """ A method that takes the title and amount as parameters and updates the title and amount attributes.If the update method is called without any parameters, the title and amount attributes remain unchanged. The updated_at attribute is updated to the current date and time in UTC.
+        """
         if title:
             self.title = title
         if amount:
@@ -25,7 +23,8 @@ class Expense:
         self.updated_at = datetime.now(timezone.utc)
 
     def to_dict(self):
-        """ A method that returns a dictionary of the set of values inputted after the update method is called."""
+        """ A method that returns a dictionary of the set of values inputted after the update method is called.
+        """
         return {
             'id': self.id,
             'title': self.title,
@@ -33,41 +32,52 @@ class Expense:
             'created_at': self.created_at.strftime('%Y-%m-%d %H:%M:%S UTC'),
             'updated_at': self.updated_at.strftime('%Y-%m-%d %H:%M:%S UTC')
         }
-        
-
-
+    
 class ExpenseDatabase:
 
-    """ A class representing a collection of expenses. The class contains an __init__ method that initializes an empty list of expenses.The class also contains an add_expense method that takes an expense as a parameter and adds it to the list of expenses. It also contains an remove_expense method that takes an expense_id as a parameter and removes the expense with the given id from the list of expenses. The get_expense_by_id method takes an expense_id as a parameter and returns the expense with the given id. Also, the get_expense_by_title method takes an expense_title as a parameter and returns the expense with the given title. The to_dict method returns a copy of the list of expenses.
-    """
+    
+
     def __init__(self):
-        """ A method that initializes an empty list of expenses."""
-        self.expenses = []
-        
+        """ A method that initializes an empty list of expenses.
+        """
+        self.database = []
+
+
 
     def add_expense(self, expense):
-        """ A method that takes an expense as a parameter and adds it to the list of expenses."""
-        self.expenses.append(expense)
+        """ A method that takes an expense as a parameter and adds it to the list of expenses.
+        """
+        self.database.append(expense)
     
+
     def remove_expense(self, expense_id):
-        """ A method that takes an expense_id as a parameter and removes the expense with the given id from the list of expenses."""
-        for expense in self.expenses:
+        """ A method that takes an expense_id as a parameter and removes the expense with the given id from the list of expenses.
+        """
+        for expense in self.database:
             if expense['id'] == expense_id:
-                self.expenses.remove(expense)
+                self.database.remove(expense)
                 break
     
+
     def get_expense_by_id(self, expense_id):
-        """ A method that takes an expense_id as a parameter and returns the expense with the given id."""
-        for expense in self.expenses:
+        """ A method that takes an expense_id as a parameter and returns the expense with the given id.
+        """
+        for expense in self.database:
             if expense['id'] == expense_id:
-                return expense
+                return expense[0]
         return None
 
 
     def get_expense_by_title(self, expense_title):
-        """ A method that takes an expense_title as a parameter and returns the expense with the given title."""
-        return [expense for expense in self.expenses if expense['title'] == expense_title]
+        """ A method that takes an expense_title as a parameter and returns the expense with the given title.
+        """
+        for expense in self.database:
+            if expense['title'] == expense_title:
+                return expense[0]
+        return None
+
 
     def to_dict(self):
-        """ A method that returns a copy of the list of expenses."""
-        return self.expenses.copy()
+        """ A method that returns a copy of the list of expenses.
+        """
+        return self.database.copy()
